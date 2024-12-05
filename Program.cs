1ca23706bf20d766449480f43ec6a914b1c5c160
Kharristov <annawey486@gmail.com>
2024-12-05 15:23:48 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static int Persistence(int num)
+    {
+        int count = 0;
+
+        while (num >= 10)
+        {
+            int product = 1;
+
+            while (num > 0)
+            {
+                product *= num % 10;  
+                num /= 10;  
+            }
+
+            num = product; 
+            count++;  
+        }
+
+        return count;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число для вычисления мультипликативной стойкости:");
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Мультипликативная стойкость: {Persistence(num)}");
+    }
+}
